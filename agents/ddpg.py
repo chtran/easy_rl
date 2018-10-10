d015e2c4dc98b252981b0fa1e0dacd00bf1c61f5
Chau Tran <chau_tran@alumni.brown.edu>
2018-10-09 21:25:04 -0700

    cosmetic update

--- agents/ddpg.py
+++ agents/ddpg.py
@@ -17,6 +17,7 @@ class DDPGAgent(Agent):
         super().__init__(env, debug)
         self.actor = ActorEstimator(env)
         self.critic = CriticEstimator(env)
+
         self._discount = discount
         self._train_iters = train_iters
         self._batch_train_iters = batch_train_iters
@@ -76,18 +77,17 @@ class DDPGAgent(Agent):
 
 
 class ActorEstimator(object):
-    def __init__(self, env, soft_update=0.99):
-        self.soft_update = soft_update
-        action_space = env.action_space.shape[0] # |A|
-        obs_space = env.observation_space.shape[0] # |S|
-        self.action_lb = env.action_space.low # |A|
-        self.action_ub = env.action_space.high # |A|
+    def __init__(self, env, soft_update_weight=0.9):
         self.estimator = LinearEstimator(obs_space, action_space)
         self.target_estimator = LinearEstimator(
             obs_space, action_space, W=self.estimator.W)
+
+        self._soft_update_weight = soft_update_weight
+        self._action_lb = env.action_space.low # |A|
+        self._action_ub = env.action_space.high # |A|
     
     def soft_update(self):
-        self.target_estimator.W += self.soft_update * (self.estimator.W - self.target_estimator.W)
+        self.target_estimator.W += self._soft_update_weight * (self.estimator.W - self.target_estimator.W)
 
     def get_action(self, state, explore_prob=0.):
         """Return dim: |A|"""
@@ -98,25 +98,26 @@ class ActorEstimator(object):
     
 
 class CriticEstimator(object):
-    def __init__(self, env):
-        self.action_space = env.action_space.shape[0] # |A|
-        self.obs_space = env.observation_space.shape[0] # |S|
+    def __init__(self, env, soft_update_weight=0.9):
         self.estimator = LinearEstimator(self.obs_space + self.action_space, 1)
         self.target_estimator = LinearEstimator(
             self.obs_space + self.action_space, 1, W=self.estimator.W)
 
+        self._soft_update_weight = soft_update_weight
+        self._action_space = env.action_space.shape[0] # |A|
+        self._obs_space = env.observation_space.shape[0] # |S|
+
     def soft_update(self):
-        self.target_estimator.W += 0.9 * (self.estimator.W - self.target_estimator.W)
+        self.target_estimator.W += self._soft_update_weight * (self.estimator.W - self.target_estimator.W)
 
     def evaluate(self, state, action):
         return self.target_estimator.predict(np.concatenate((state.flatten(), action)))
 
     def action_grad(self, action):
         # |A|
-        grad = self.estimator.partial_grad(self.obs_space, self.obs_space + self.action_space)
+        grad = self.estimator.partial_grad(self._obs_space, self._obs_space + self._action_space)
         return grad
 
     def fit(self, state, action, target):
-        lets_print = random.random() < 0.001
         x = np.concatenate((state.flatten(), action))
         self.estimator.fit(x, target)
